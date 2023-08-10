1d602a967cd287502f85ea9ed0bb9b3d684ddd11
Kirandeep2806 <kirandeep102030@gmail.com>
2023-08-10 22:43:31 +0530

    Responsive design of MUI

--- src/components/DietCreation.js
+++ src/components/DietCreation.js
@@ -21,23 +21,19 @@ const DietCreation = () => {
 
     return (
         <Grid container spacing={2} className="diet-creation-container">
-            <Grid item lg={2} xl={2} md={3} sm={3} xs={4}>
-                {/* <Item> */}
+            <Grid item lg={2} xl={2} md={3} sm={3} xs={3}>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <TimePicker label="Enter Time" defaultValue={dayjs(time)} onChange={(val) => handleTimeChange(val)}  className="components"/>
                         {/* <p>{time}</p> */}
                     </LocalizationProvider>
-                {/* </Item> */}
             </Grid>
-            <Grid item lg={8} xl={9} md={7} sm={7} xs={6}>
+            <Grid item lg={9} xl={9} md={7} sm={6} xs={5}>
                 <TextField variant="outlined" label="Enter the diet" required className="components"></TextField>
             </Grid>
-            <Grid item lg={1} xs={1} sm={1}>
+            <Grid item lg={1} xl={1} xs={4} sm={3} md={2}>
                 <IconButton aria-label="add-new-entry" className="crud-icons">
                     <AddOutlined color="primary" fontSize="medium"/>
                 </IconButton>
-            </Grid>
-            <Grid item lg={1} xs={1} sm={1}>
                 <IconButton aria-label="delete-entry" className="crud-icons">
                     <DeleteOutline color="error" fontSize="medium" />
                 </IconButton>
